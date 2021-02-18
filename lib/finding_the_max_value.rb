9e87842ef6785618e6170509f15b0b7b7bf9094a
casual-karma-e108 <akravecas@yahoo.com>
2021-02-18 19:33:55 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,10 @@
+require 'try'
+
 def find_max_value(array)
   count = 0
   while count < array.length
     if array[count] > array[count+1]
     p array[count+1]
+    binding.pry
     count +=1
 end
